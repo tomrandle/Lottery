b852617cae887143e764d024cf703df1bf27ffb5
Tom Randle <tom.randle@gmail.com>
2012-08-15 10:27:49 +0100

    Now working!

--- lottery.py
+++ lottery.py
@@ -1,28 +1,37 @@
 import random
 
-numbers = []
+winners = 0
 
-for i in range(1,50):
-	numbers.append(i)
-print numbers
+for i in range (1,10000000):
 
+	numbers = []
 
-ticket  = []
-
-for i in range (1,7):
-	remainingnumbers = len(numbers)
-
-	selectednumber = random.randrange(1,remainingnumbers)
-	numberindex = numbers.index(selectednumber)
-
-	del numbers[i]
+	for i in range(1,50):
+		numbers.append(i)
 
 
+	ticket  = []
 
-	ticket.append(selectednumber)
+	for i in range (1,7):
+		remainingnumbers = len(numbers)
 
-ticket.sort()
-print ticket	
+		numberindex = random.randrange(0,remainingnumbers)
+		
+		selectednumber = numbers[numberindex]
 
+
+		del numbers[numberindex]
+
+
+
+		ticket.append(selectednumber)
+
+	ticket.sort()
+	#print ticket	
+
+	if ticket == [1,2,3,4,5,6]:
+		winners += 1
+
+print "Number of winners: %d" % winners
 
 
